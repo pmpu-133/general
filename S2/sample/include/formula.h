eeb3a3bc90b76f161ba2edf3a5e546f907e47479
Чакляров Георгий Олегович <st069378@ad.pu.ru>
2019-05-06 16:00:35 +0300

    Edit formula.h

--- S2/sample/include/formula.h
+++ S2/sample/include/formula.h
@@ -6,7 +6,13 @@
 
 class FormulaNode {
 public:
-  FormulaNode(char c) {
+  FormulaNode() {
+    m_number = 0;
+    m_oper = SUM;
+    m_isOperator = false;
+  }
+  
+  FormulaNode(Operators c) {
     m_number = 0;
     m_oper = c;
     m_isOperator = true;
@@ -14,8 +20,21 @@ public:
 
   FormulaNode(double d) {
     m_number = d;
-    m_oper = 0;
+    m_oper = SUM;
     m_isOperator = false;
+  }
+
+  FormulaNode(char op) {
+    switch (op) {
+    case '+': m_oper = SUM; break;
+    case '-': m_oper = SUB; break;
+    case '*': m_oper = MUL; break;
+    case '/': m_oper = DIV; break;
+    case '(': m_oper = OPEN_BRACKET; break;
+    case ')': m_oper = CLOSE_BRACKET; break;
+    }
+    m_number = 0;
+    m_isOperator = true;
   }
 
   bool isOperator()const {
@@ -23,7 +42,7 @@ public:
   }
 
   bool isOperand()const {
-    return m_isOperator;
+    return !m_isOperator;
   }
 
   double value()const {
@@ -32,23 +51,60 @@ public:
     else throw std::logic_error("It's not a number");
   }
 
-  char setOp(char c) {
-    m_oper = c;
+  Operators oper()const {
+    if (m_isOperator)
+      return m_oper;
+    else
+      throw std::logic_error("Not an operator");
   }
+
+  
 
 private:
   double m_number;
-  char m_oper;
+  Operators m_oper;
   bool m_isOperator;
 };
 
 
+std::ostream& operator<< (std::ostream& ost, const FormulaNode& node) {
+  if (node.isOperand())
+    ost << node.value();
+  else {
+    switch (node.oper()) {
+    case SUM: ost << '+'; break;
+    case SUB: ost << '-'; break;
+    case MUL: ost << '*'; break;
+    case DIV: ost << '/'; break;
+    case OPEN_BRACKET: ost << '('; break;
+    case CLOSE_BRACKET: ost << ')'; break;
+    }
+  }
+  return ost;
+}
+
+
+bool operator<(const Operators& op1, const Operators& op2) {
+  if (op1 == DIV || op1 == MUL)
+    return true;
+  if (op1 == SUM || op1 == SUB)
+    if (op2 == DIV || op2 == MUL)
+      return false;
+    else
+      return true;
+}
+
 template<typename Q> class FormulaParser {
 public:
-  void setResult(Q *q);
+  FormulaParser() { m_qu = nullptr;}
+  void setResult(Q *q) {  m_qu = q; }
+
+ 
 
   void parse(const char* formula) {
-    for (int i = 0; i < formula.size(); ++i) {
+    if (m_qu == nullptr)
+      throw std::runtime_error("FormulaParser: have no outpit queue");
+    for (int i = 0; i < strlen(formula); ++i) {
       double ch = 0;
       if (formula[i] >= 48 && formula[i] <= 57) {
         ch = ch * 10;
@@ -62,8 +118,15 @@ public:
           ch += (formula[i] - '0') * pow(10, counter);
         }
       }
-      FormulaNode o(ch);
-      m_qu.push(o);
+      if (formula[i] == '+' || formula[i] == '-' || formula[i] == '*' || formula[i] == '/' || formula[i] == ')' || formula[i] == '(')
+      {
+        FormulaNode node(formula[i]);
+        m_qu->push(node);
+      }
+      else {
+        FormulaNode node(ch);
+        m_qu->push(node);
+      }
     }
   }
 
@@ -73,111 +136,44 @@ private:
 
 
 template<typename Q> class DijkstraSorter {
-  void setInput(Q*);
-  void setOutput(Q*);
-  void run();
+  void setInput(Q*q) { m_input = q;}
+  void setOutput(Q*q) { m_output = q;}
+  void run() {
+    while (!m_input->empty()) {
+      FormulaNode node = m_input->getFirst();
+      if (node.isOperand) {
+        m_output->push(node);
+      }
+      else {
+        if (m_sta.empte())
+          m_sta.push(node);
+        else {
+          if (node.oper() > m_sta.looktop().oper()) {
+            m_sta.push(node);
+          }
+          else {
+            while (m_sta.empty() && node.oper() > !(m_sta.looktop().oper())) {
+              m_output->push(m_sta.looktop());
+              m_sta.pop();
+            }
+          }
+        }
+      }
+    }
+  }
 private:
-
+  Q* m_input;
+  Q* m_output;
+  stakk<FormulaNode> m_sta;
 };
 
 
 template<typename Q>  double evaluate(Q&q) {
   while (!q.empty()) {
-
+    
   }
 };
 
 
-Operators Compare(const Operators& op1, const Operators& op2) {
-  if (op1 == DIV || op1 == MUL)
-      return op1;
-  if (op1 == SUM || op1 == SUB)
-    if (op2 == DIV || op2 == MUL)
-      return op2;
-    else
-      return op1;
-}
 
 
-/*void fromBaseToPost(string str) {
-  stakk<Operators> stack;
-  qu<FormulaNode> queue;
-  for (int i = 0; i < str.size(); ++i) {
-    double ch = 0;
-    if (str[i] >= 48 && str[i] <= 57) {
-      ch = ch * 10;
-      ch += str[i] - '0';
-    }
-    int counter = 0;
-    if (str[i] == ',') {
-      ++i;
-      while (str[i] != '+' || str[i] != '-' || str[i] != '*' || str[i] != '/') {
-        counter--;
-        ch += (str[i] - '0') * pow(10, counter);
-      }
-    }
-    FormulaNode o(ch);
-    queue.push(o);
-    if (str[i] != '+' || str[i] != '-' || str[i] != '*' || str[i] != '/' || str[i] == '(' || str[i] == ')') {
-      if (stack.empty())
-        switch (str[i]) {
-        case'+':
-          stack.push(SUM);
-        case'-':
-          stack.push(SUB);
-        case'*':
-          stack.push(MUL);
-        case'/':
-          stack.push(DIV);
-        case'(':
-          stack.push(OPEN_BRACKET);
-        }
-      else {
-        switch (str[i]) {
-        case'+':
-          if (Compare(SUM, stack.looktop()))
-            stack.push(SUM);
-          else
-            while (!stack.empty()) {
-              FormulaNode o;
-              o.setOp(stack.looktop());
-              queue.push(o);
-              stack.pop();
-            }
-        case'-':
-          if (Compare(SUB, stack.looktop()))
-            stack.push(SUB);
-          else
-            while (!stack.empty()) {
-              FormulaNode o;
-              o.setOp(stack.looktop());
-              queue.push(o);
-              stack.pop();
-            }
-        case'*':
-          if (Compare(MUL, stack.looktop()))
-            stack.push(MUL);
-        case'/':
-          if (Compare(MUL, stack.looktop()))
-            stack.push(DIV);
-        case'(':
-          stack.push(OPEN_BRACKET);
-        case')':
-          while (stack.looktop() != ')') {
-            FormulaNode o;
-            o.setOp(stack.looktop());
-            queue.push(o);
-            stack.pop();
-          }
-          stack.pop();
-        }
-      }
-    }
-  }
-  while (!stack.empty()) {
-    FormulaNode o;
-    o.setOp(stack.looktop());
-    queue.push(o);
-    stack.pop();
-  }
-}*/
